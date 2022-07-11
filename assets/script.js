ead4702964aa9f009855c6756fd8e54d6ced659c
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-11 13:50:26 -0500

    changed the coordinates to be DFW

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,22 @@
-function initGoogle() {
-    var location = {
-        lat: 32.7767,
-        lng: -96.7970
-    }
-    var options = {
-        center: location,
-        zoom: 8
-    }
-    map = new google.maps.Map(document.getElementById("map"), options);
+// function initGoogle() {
+//     var location = {
+//         lat: 32.7767,
+//         lng: -96.7970
+//     }
+//     var options = {
+//         center: location,
+//         zoom: 8
+//     }
+//     map = new google.maps.Map(document.getElementById("map"), options);
+// }
+
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 32.7767, lng: -96.7970 },
+    zoom: 8,
+  });
 }
+
+window.initMap = initMap;
