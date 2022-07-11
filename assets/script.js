a03fe106658a764a26b01b5c205871edc233cc1b
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-11 14:37:05 -0500

    changes to input

--- assets/script.js
+++ assets/script.js
@@ -17,6 +17,33 @@ function initMap() {
     center: { lat: 32.7767, lng: -96.7970 },
     zoom: 11,
   });
+
+  
+
 }
 
 window.initMap = initMap;
+
+// if (navigator.geolocation) {
+//     console.log('geolocation is here!');
+
+//     navigator.geolocation.getCurrentPosition((loc) => {
+//         location.lat = loc.coords.latitude;
+//         location.lng = loc.coords.longitude;
+
+//         //Write the map
+//         map = new google.maps.Map(document.getElementById("map"), options);
+//     })
+// } else {
+//     console.log('geolocation not supported');
+//     map = new google.maps.Map(document.getElementById("map"), options);
+// }
+
+// autocomplete = new google.maps.places.Autocomplete(document.getElementById("input"),  
+// {
+//     componentRestrictions: {'country' : ['us']}, 
+//     fields: ['geometry', 'name'],
+//     types: ['city']
+// })
+
+// autocomplete.addListener("place_changed")
