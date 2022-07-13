b30a914b5a3a045b1f9b2df45dc7acd8f6ad9f13
ajcodes-cyber <anthonyjohn350@gmail.com>
2022-07-13 12:33:31 -0500

    added Sunrise Sunset api and started html for the sunrise sunset section

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-
+// Google Maps API
 let map;
 
 function initMap() {
@@ -13,39 +13,7 @@ function initMap() {
 
 window.initMap = initMap;
 
-// if (navigator.geolocation) {
-//     console.log('geolocation is here!');
-
-//     navigator.geolocation.getCurrentPosition((loc) => {
-//         location.lat = loc.coords.latitude;
-//         location.lng = loc.coords.longitude;
-
-//         //Write the map
-//         map = new google.maps.Map(document.getElementById("map"), options);
-//     })
-// } else {
-//     console.log('geolocation not supported');
-//     map = new google.maps.Map(document.getElementById("map"), options);
-// }
-
-// autocomplete = new google.maps.places.Autocomplete(document.getElementById("input"),  
-// {
-//     componentRestrictions: {'country' : ['us']}, 
-//     fields: ['geometry', 'name'],
-//     types: ['city']
-// })
-
-// autocomplete.addListener("place_changed")
-
-// let sunriseSunset;
-
-// fetch('https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&callback=mycallback') {
-//     .then(response => response.json())
-//   .then(data => console.log(data));
-// }
-  
-// fetch ("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=214556dfb689253a62f7c088b67992ee")
-
+// Random Dog Image Generator API
 const dogBtn = document.querySelector('.dogBtn');
 const dogImage = document.querySelector('.dogImage');
 dogBtn.addEventListener('click',()=>{
@@ -57,3 +25,14 @@ dogBtn.addEventListener('click',()=>{
     dogImage.src = picture.message;
   });
 });
+
+// Sunrise Sunset API
+const getData = () => {
+    fetch ("https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today")
+    .then(data => data.json())
+    .then((data) => console.log(data));
+    // document.querySelector('#name').innerHTML = '<h1>'
+    
+};
+
+getData();
